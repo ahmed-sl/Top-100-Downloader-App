4e77a05ba1adee9bfc19c27351c35cbd1a4fd7d2
aalsu <aalsulami35@gmail.com>
2021-10-21 08:49:50 +0300

    Top 100 Downloader App

---
+++ app/src/main/java/com/example/top100downloaderapp/FeedAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.top100downloaderapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.top100downloaderapp.databinding.RvFeedItemBinding
+
+class FeedAdapter (private val items:ArrayList<FeedEntry>): RecyclerView.Adapter<FeedAdapter.MyViewHolder>() {
+    class MyViewHolder(val binding: RvFeedItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        return MyViewHolder(RvFeedItemBinding.inflate(LayoutInflater.from(parent.context),
+            parent,
+            false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val title =items[position].name
+        holder.binding.apply {
+            tvFeed.text = title
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
